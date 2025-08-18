1fd67f8dcef78aa10b9b03d6475c2b2e873a5fec
leticiaa-santos <43705577808@>
2025-08-18 11:32:26 -0300

    projeto com inicio em sass

---
+++ kanban/kanban/src/App.jsx
@@ -0,0 +1,12 @@
+import './Style/main.scss';
+import { Quadro } from './Componentes/Quadro';
+
+function App() {
+  
+
+  return (
+    <Quadro/>
+  );
+}
+
+export default App
---
+++ kanban/kanban/src/Componentes/Quadro.jsx
@@ -0,0 +1,14 @@
+import { Coluna } from "./Coluna";
+
+export function Quadro () {
+    return (
+
+        <main className="container">
+            <h1>Meu Quadro</h1>
+            <Coluna/>
+        </main>
+        
+        
+    );
+
+}
